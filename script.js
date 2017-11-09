3384c3e8497b2eccb3dfdd7b68f1137b6d56f673
David Bearchell <dbearchell@gmail.com>
2017-11-08 23:42:27 -0500

    test commit

---
+++ script.js
@@ -0,0 +1,4 @@
+
+for(i=0;i<10;i++){
+document.write('<div class="py-5"><a href="#!" class="btn btn-primary">Default Button</a><a href="#!" class="btn btn-outline-primary">Outline Button</a><a href="#!" class="btn btn-outline-primary"><span class="fa fa-play mr-2"></span>Start</a></div><br/>');
+}
